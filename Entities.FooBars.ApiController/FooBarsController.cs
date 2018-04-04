6ccae250319320c1916f716229ce73088252d79c
Christopher Dorst <chris@dorst.email>
2018-04-03 19:21:09 -0500

    Update FooBarsController.cs

--- Entities.FooBars.ApiController/FooBarsController.cs
+++ Entities.FooBars.ApiController/FooBarsController.cs
@@ -3,6 +3,7 @@
 
 using DevOps.Code.DataAccess.Interfaces.Repository;
 using Entities.FooBars.DatabaseContext;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
